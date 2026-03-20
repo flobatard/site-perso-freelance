231f5d668297d27e7fbce3a842a91ce1f63ce3af
flobatard <fb.batard@gmail.com>
2026-03-20 09:17:10 +0100

    Working chunking

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,17 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.includes("node_modules")) return;
+          if (id.includes("@radix-ui")) return "radix-ui";
+          if (id.includes("@tanstack")) return "query";
+        },
+      },
+    },
+  },
   ssr: {
     noExternal: ["react-router-dom"],
   },
